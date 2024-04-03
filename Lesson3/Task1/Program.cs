0de310b61d45806bd33ea525a8a882736ca610ab
Roman Cheremuhin <cheremuhinrv@gmail.com>
2024-04-03 23:28:37 +0300

    add Lesson 1-4

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double resalt = numenator / denomenator;
+    return resalt;
+}
+
+double resalt = CalculateFormula(1, 2, 3, 4);
+System.Console.WriteLine(resalt);
